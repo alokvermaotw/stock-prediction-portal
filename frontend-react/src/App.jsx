8ddeb23b0a153f876ab0f19471b92071e3a3b639
Alok Verma <alok.verma@neomediaworld.com>
2025-05-23 09:39:53 +0530

    Homepage

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
+import './assets/css/style.css'
+import Footers from './assets/components/Footers'
+import Headers from './assets/components/Headers'
+import Main from './assets/components/Main'
 
 function App() {
 
   return (
     <>
-      
+    <Headers />
+    <Main />
+    <Footers />
     </>
   )
 }
---
+++ frontend-react/src/assets/components/Footers.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footers = () => {
+  return (
+    <>
+      <footer className='footer py-3 my-3'>
+        <hr className='border-bottom' />
+
+        <p className='text-light text-center'>&copy; 2024 - Build with Love ❤️ by Alok Verma</p>
+      </footer>    
+    </>
+  )
+}
+
+export default Footers
